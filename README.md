c5974dd2bcc48302cce1c349c9ea239aedcde7a9
kostya123 <kkkk.hhhh.03@mail.ru>
2023-11-21 15:29:34 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# hovhannesandsara
+# asdg
---
+++ js/main.js
@@ -0,0 +1,67 @@
+const timer = document.getElementById("timer");
+console.log(timer);
+
+const countDown = new Date("nov 25, 2023 13:39:0").getTime();
+
+const update = setInterval(function () {
+  const now = new Date().getTime();
+
+  let diff = countDown - now;
+
+  //now we are calculating time in days, hrs, minutes, and seconds.
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hrs = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  timer.innerHTML = `
+    <div class="item">
+        <h2>${days}</h2>
+        <span>D</span>
+    </div>
+    <div class="item">
+        <h2>:</h2>
+    </div>
+    <div class="item">
+        <h2>${hrs}</h2>
+        <span>H</span>
+    </div>
+    <div class="item">
+        <h2>:</h2>
+    </div>
+    <div class="item">
+        <h2>${minutes}</h2>
+        <span>M</span>
+    </div>
+    <div class="item">
+        <h2>:</h2>
+    </div>
+    <div class="item">
+        <h2>${seconds}</h2>
+        <span>S</span>
+    </div>
+   `;
+
+  if (days < 0 && hrs < 0 && minutes < 0 && seconds < 0) {
+    clearInterval(update);
+    
+    timer.innerHTML = `
+    <div class="item">
+        <h2>0</h2>
+        <span>D</span>
+    </div>
+    <div class="item">
+        <h2>0</h2>
+        <span>H</span>
+    </div>
+    <div class="item">
+        <h2>0</h2>
+        <span>M</span>
+    </div>
+    <div class="item">
+        <h2>0</h2>
+        <span>S</span>
+    </div>
+   `;
+  }
+}, 1000);
